9d609847c29e0e7f663b592c3f7fde761fc09127
Ansh Gujral <anshgujral1997@gmail.com>
2018-03-22 00:53:49 +0530

    few formattings done

--- src/screens/mainTab.js
+++ src/screens/mainTab.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import {startSingleScreenApplicationLogin} from '../styles/navigatorStyles';
-import { Container, Header, Content, List, ListItem,Title } from 'native-base';
+import { Container, Header, Content, List, ListItem,Title, StyleProvider } from 'native-base';
+import material from '../../native-base-theme/variables/material';
+import getTheme from '../../native-base-theme/components';
 import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json
 import UCS401 from './tabscreens/UCS401';
 import UTA007 from './tabscreens/UTA007';
@@ -12,21 +14,31 @@ const Tabs = TabNavigator({
   UCS401: { screen: UCS401 },
   UES100: { screen: UTA007},
   UTA007: { screen: UTA007},
-  UCS402: { screen: UCS402}
+  UCS402: { screen: UCS402},
+},
+{
+ tabBarOptions: {
+       style: {
+      backgroundColor: '#37383a',
+    },
+  }
 });
 
 
 export default class mainTab extends Component {
       static navigatorStyle=startSingleScreenApplicationLogin;
+
       render()
       {
             return(
+                  <StyleProvider style={getTheme(material)}>
                   <Container>
                     <Header>
                       <Title>Attendance Manager</Title>
                     </Header>
                     <Tabs />
               </Container>
+        </StyleProvider>
             );
       }
 }
